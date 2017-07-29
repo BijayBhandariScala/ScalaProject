3a5ad1fd9b2b258ed6f0afabf7f9d3dfcf912a20
Bijay Bhandari <bijay.bhandari007@gmail.com>
2017-07-29 09:06:19 -0400

    Adding variable class to define variable declaration.

---
+++ src/com/bijay/scala/tutorial/Variable.scala
@@ -0,0 +1,21 @@
+package com.bijay.scala.tutorial
+
+object Variable {
+  
+   var data=100   //1st way
+   //data= 101
+   
+  
+  def main(args: Array[String]) {
+      println("I am variable:") 
+      println(data)
+      value
+      
+  }
+   
+   def value{            
+      val data:Int= 200    //2nd way
+      print(data)
+   }
+  
+}
